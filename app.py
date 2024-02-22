6b79209b41b5487be437362b24e5339e020bce52
Jason Zenarosa <jasonzenarosa@gmail.com>
2024-02-21 18:36:09 -0800

    serverless setup

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-import psycopg2, psycopg2.extras, responses, tsdb
+import assets.responses as responses, assets.tsdb as tsdb
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/bin/<int:bin_id>", methods=["GET"])
 def get_bin(bin_id:int):
-    global conn
+    conn = tsdb.connect()
     query = f"SELECT * from ts_bins WHERE bin_id = '{bin_id}'"
     bins = [dict(row) for row in tsdb.execute_query(conn, query)]
     response = responses.get_bin_response(bins, bin_id)
     return jsonify(response)
-
-if __name__ == "__main__":
-    conn = tsdb.connect()
-    app.run(port=5000)
