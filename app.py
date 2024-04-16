94afc009c771c13fd1403bdf019ed6ad1c063909
ManjotSingh18 <manjos2@uci.edu>
2024-04-16 15:29:50 -0700

    ZZ

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import assets.responses as responses, assets.tsdb as tsdb
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
+'''
 @app.route("/bin/<int:bin_id>", methods=["GET"])
 def get_bin(bin_id:int):
     conn = tsdb.connect()
@@ -10,3 +11,18 @@ def get_bin(bin_id:int):
     bins = [dict(row) for row in tsdb.execute_query(conn, query)]
     response = responses.get_bin_response(bins, bin_id)
     return jsonify(response)
+'''
+
+@app.route("/")
+def hello_from_root():
+    return jsonify(message='Hello from root!')
+
+
+@app.route("/hello")
+def hello():
+    return jsonify(message='Hello from path!')
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 404)
