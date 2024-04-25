7a5edc88bcf3a5a405260b2873a5359ac5088f5a
ManjotSingh18 <manjos2@uci.edu>
2024-04-25 14:25:29 -0700

    Added Database foundations

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from flask import Flask, jsonify, make_response
 # Test Comment for Push
 app = Flask(__name__)
 
-'''
 @app.route("/bin/<int:bin_id>", methods=["GET"])
 def get_bin(bin_id:int):
     conn = tsdb.connect()
-    query = f"SELECT * from ts_bins WHERE bin_id = '{bin_id}'"
+    query = f"SELECT * from ts_bins_info NATURAL JOIN ts_sensor_data WHERE bin_id = {bin_id};"
     bins = [dict(row) for row in tsdb.execute_query(conn, query)]
     response = responses.get_bin_response(bins, bin_id)
+    conn.close() 
     return jsonify(response)
-'''
 
 @app.route("/")
 def hello_from_root():
--- assets/tsdb.py
+++ assets/tsdb.py
@@ -1,16 +1,13 @@
-"""
-import env, psycopg2, psycopg2.extras
+import os, psycopg2, psycopg2.extras
 
 def connect():
-    db_name = env.DB_NAME
-    db_user = env.DB_USER
-    db_host = env.DB_HOST
-    db_port = env.DB_PORT
-    db_pass = env.DB_PASS
+    db_name = os.environ['DB_NAME']
+    db_user = os.environ['DB_USER']
+    db_host = os.environ['DB_HOST']
+    db_port = os.environ['DB_PORT']
+    db_pass = os.environ['DB_PASS']
 
-    print("Attempting to connect to database...")
     conn = psycopg2.connect(user=db_user, database=db_name, host=db_host, password=db_pass, port=db_port)
-    print("Connection established!")
 
     return conn
 
@@ -18,4 +15,3 @@ def execute_query(conn, query):
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute(query)
     return cursor.fetchall()
-"""
