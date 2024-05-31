4d97675c9dd26bd20c5a7dc51b8e7b1fede6775f
ManjotSingh18 <manjos2@uci.edu>
2024-05-31 04:50:57 -0700

    Fixed get_usage endpoint

--- app.py
+++ app.py
@@ -1,14 +1,74 @@
 import assets.responses as responses, assets.tsdb as tsdb
-from flask import Flask, jsonify, make_response
-# Test Comment for Push
-app = Flask(__name__)
+from flask import Flask, jsonify, make_response, request
+from datetime import datetime
+import time
+from flask_cors import CORS
 
+
+app = Flask(__name__)
+CORS(app)
+
+FORMAT_DATA = "%Y-%m-%d %H:%M:%S%z"
 @app.route("/bin/<int:bin_id>", methods=["GET"])
 def get_bin(bin_id:int):
+    
     conn = tsdb.connect()
     query = f"SELECT * from ts_bins_info NATURAL JOIN ts_sensor_data WHERE bin_id = {bin_id};"
     bins = [dict(row) for row in tsdb.execute_query(conn, query)]
     response = responses.get_bin_response(bins, bin_id)
+    conn.close() 
+
+
+    return jsonify(response)
+
+@app.route("/get_location/<int:bin_id>", methods=["GET"])
+def get_location(bin_id:int):
+    conn = tsdb.connect()
+    query = f"SELECT latitude, longitude from ts_bins_info WHERE bin_id = {bin_id};"
+    bins = [dict(row) for row in tsdb.execute_query(conn, query)]
+    response = responses.get_bin_response(bins, bin_id)
+    conn.close()
+
+
+
+    return jsonify(response)
+
+@app.route("/get_bins", methods=["GET"])
+def get_bins():
+    conn = tsdb.connect()
+    query = f"SELECT * from ts_bins_info;"
+    bins = [dict(row) for row in tsdb.execute_query(conn, query)]
+    response = responses.get_bins_response(bins)
+    conn.close()
+
+
+    
+
+    return jsonify(response)
+
+@app.route("/get_usage/<int:bin_id>", methods=["GET"])
+def get_usage(bin_id:int):
+    start = request.args.get('start')
+    end = request.args.get('end')
+    valid_date = True
+    conn = tsdb.connect()
+    query = f"SELECT usage, timestamp FROM ts_sensor_data WHERE bin_id = {bin_id}"
+    if start != None:
+        start_timestamp = datetime.strptime(start, FORMAT_DATA)
+        query += f" AND timestamp >= '{start_timestamp}'"
+    if end != None:
+        end_timestamp = datetime.strptime(end, FORMAT_DATA)
+        query += f" AND timestamp < '{end_timestamp}'"
+    query+=';'
+    if (start!=None and end !=None) and start_timestamp > end_timestamp:
+        valid_date = False
+    bins = [dict(row) for row in tsdb.execute_query(conn, query)]
+    response = responses.get_usage_response(bins, bin_id, valid_date)
+    if len(response )== 0:
+        single_query = f"SELECT * from ts_bins_info NATURAL JOIN ts_sensor_data WHERE bin_id = {bin_id};"
+        single_bin = [dict(row) for row in tsdb.execute_query(conn, single_query)]
+        response = responses.get_bin_response(single_bin, bin_id)
+        
     conn.close() 
     return jsonify(response)
 
@@ -25,3 +85,4 @@ def hello():
 @app.errorhandler(404)
 def resource_not_found(e):
     return make_response(jsonify(error='Not found!'), 404)
+
--- assets/responses.py
+++ assets/responses.py
@@ -1,6 +1,7 @@
 import json
 
 def get_bin_response(bins, bin_id):
+
     if len(bins) == 0:
         return {
             'statusCode': 404,
@@ -20,3 +21,54 @@ def get_bin_response(bins, bin_id):
             }
         }
     
+#Get location shares this handler to avoid redundancy.
+def get_bins_response(bins):
+    if len(bins) == 0:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({
+                'detail': f'Bins not found'
+            }),
+            'headers': {
+                "Content-Type": "application/json"
+            }
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps(bins, default=str),
+            'headers': {
+                "Content-Type": "application/json"
+            }
+        }
+    
+def get_usage_response(bins, bin_id, valid_date):
+    if valid_date == False:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({
+                'detail': f'Invalid Dates'
+            }),
+            'headers': {
+                "Content-Type": "application/json"
+            }
+        }
+    if len(bins) == 0:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({
+                'detail': f'No data found for bin: {bin_id}'
+            }),
+            'headers': {
+                "Content-Type": "application/json"
+            }
+        }
+    
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps(bins, default=str),
+            'headers': {
+                "Content-Type": "application/json"
+            }
+        }
